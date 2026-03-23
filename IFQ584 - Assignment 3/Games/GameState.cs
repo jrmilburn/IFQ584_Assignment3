6efd7baaa5a8a381c210451073f9c9debdf52397
brentoj <brenton.jermalonek@gmail.com>
2026-03-24 08:22:56 +1000

    Add files via upload

---
+++ IFQ584 - Assignment 3/Games/GameState.cs
@@ -0,0 +1,12 @@
+﻿namespace BoardGames
+{
+	public class GameState
+	{
+		public string GameTypeId { get; set; } = "";
+		public string Mode { get; set; } = "";
+		public string BoardData { get; set; } = "";
+		public int CurrentPlayer { get; set; }
+		public List<string> UndoMoves { get; set; } = new();
+		public List<string> RedoMoves { get; set; } = new();
+	}
+}
