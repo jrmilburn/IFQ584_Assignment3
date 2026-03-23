fca67f528f4b2bd99931c6f6f464b53373614df3
brentoj <brenton.jermalonek@gmail.com>
2026-03-24 08:27:27 +1000

    Add files via upload

---
+++ IFQ584 - Assignment 3/Moves/Move.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGames
+{
+	public class Move
+    {
+        public int PlayerId { get; }
+        public int X { get; }
+        public int Y { get; }
+        public string ValueOrPiece { get; }
+        public int BoardIndex { get; }
+
+        public Move(int playerId, int x, int y, string valueOrPiece = "X", int boardIndex = 0)
+        {
+            PlayerId = playerId;
+            X = x;
+            Y = y;
+            ValueOrPiece = valueOrPiece;
+            BoardIndex = boardIndex;
+        }
+
+        public override string ToString() =>
+            $"Player {PlayerId} → ({X},{Y}) [{ValueOrPiece}] board={BoardIndex}";
+    }
+}
---
+++ IFQ584 - Assignment 3/Moves/MoveHistory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGames
+{
+	public class MoveHistory
+	{
+		private readonly Stack<Move> _undo = new();
+		private readonly Stack<Move> _redo = new();
+
+		public int UndoCount => _undo.Count;
+		public int RedoCount => _redo.Count;
+		public Move? UndoTop => _undo.TryPeek(out var m) ? m : null;
+
+		public void DoMove(Move m)
+		{
+			_undo.Push(m);
+			_redo.Clear();          // new move clears redo
+		}
+
+		public Move? Undo()
+		{
+			if (_undo.Count == 0) return null;
+			var m = _undo.Pop();
+			_redo.Push(m);
+			return m;
+		}
+
+		public Move? Redo()
+		{
+			if (_redo.Count == 0) return null;
+			var m = _redo.Pop();
+			_undo.Push(m);
+			return m;
+		}
+
+		public void ClearRedo() => _redo.Clear();
+
+		public List<Move> UndoMoves() => _undo.ToList();
+		public List<Move> RedoMoves() => _redo.ToList();
+
+		public void Restore(List<Move> undoMoves, List<Move> redoMoves)
+		{
+			_undo.Clear(); _redo.Clear();
+			// Stacks push in reverse
+			foreach (var m in Enumerable.Reverse(undoMoves)) _undo.Push(m);
+			foreach (var m in Enumerable.Reverse(redoMoves)) _redo.Push(m);
+		}
+	}
+}
